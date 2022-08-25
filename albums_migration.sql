35bc0cd8678aa94951bf56110a00945b101f5121
katygil7 <gil.katherine22@gmail.com>
2022-08-25 16:12:33 -0500

    finished select_exercises.sql

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE albums
     id           INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_name  VARCHAR(50) NOT NULL,
     record_name  VARCHAR(100),
-    release_date DATE,
+    release_date YEAR,
     record_sale  FLOAT,
     record_genre VARCHAR(100),
     PRIMARY KEY (id)
---
+++ select_exercises.sql
@@ -0,0 +1,11 @@
+SELECT * FROM albums WHERE artist_name = 'Pink Floyd';
+
+
+SELECT release_date  FROM albums WHERE record_name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT record_name FROM albums WHERE release_date BETWEEN 1990 AND 2000;
+
+SELECT record_name From albums WHERE record_sale < 20;
+
+SELECT * FROM albums WHERE record_genre = 'rock';
+
