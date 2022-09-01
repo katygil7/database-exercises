43842eb233e863888ef7008b0132ab4ddf0e9342
katygil7 <gil.katherine22@gmail.com>
2022-09-01 10:20:20 -0500

    added an unique index for album and artist name on albums data

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,9 +1,10 @@
-# example fir aliases (as)
+# example for aliases (as)
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
 WHERE length(CONCAT(first_name, ' ', last_name))>20
 ORDER BY full_name
 LIMIT 25;
+
 # In order to use (full_name) having can be used
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
 FROM employees
@@ -11,10 +12,12 @@ HAVING length(full_name)>20
 ORDER BY full_name
 LIMIT 25;
 
+# Exercises
 #Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
 select concat(last_name, ' ', first_name) as full_name
 from employees
 limit 10;
+
 #Add the date of birth for each employee as 'DOB' to the query.
 select concat(last_name, ' ', first_name) as full_name, birth_date as DOB
 from employees
